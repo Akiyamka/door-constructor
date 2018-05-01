666986c3be2eb6ac08bbd312dfc3591ad59810f8
akiyamka <akiyamka@gmail.com>
2018-05-01 15:06:41 +0300

    add door view - 3d view

---
+++ src/components/doorView/index.vue
@@ -0,0 +1,33 @@
+<script>
+import { mapState } from 'vuex';
+import { mapMutations } from 'vuex';
+
+export default {
+  props: {},
+  components: {},
+  computed: mapState({
+    store: state => state
+  }),
+  methods: {
+    ...mapMutations(['setActiveBlock']),
+  }
+};
+</script>
+
+<template>
+  <div class="door-view">
+    <div class="door-model">
+      <div class="container">
+        <div class="side back"></div>
+        <div class="side left"></div>
+        <div class="side right"></div>
+        <div class="side top"></div>
+        <div class="side bottom"></div>
+        <div class="side front"></div>
+      </div>
+    </div>
+    <div class="controls"></div>
+  </div>
+</template>
+
+<style scoped lang='stylus' src="./index.styl"></style>
